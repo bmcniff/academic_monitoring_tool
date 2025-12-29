1302b256354ce97fe4e183054e6d110a6e0b681d
Brian McNiff <bmcniff12@gmail.com>
2025-12-29 17:45:39 -0500

    add_password_v3

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from google.oauth2.service_account import Credentials
 # Use the password stored in Streamlit secrets
 # PASSWORD = st.secrets.get("APP_PASSWORD", "changeme")
 PASSWORD = st.secrets.get("APP_PASSWORD")
+st.write("Password length:", len(pwd))
 
 # Session state to track login
 if "authenticated" not in st.session_state:
